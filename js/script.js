97c6b2f478376521d1d67061a1f15eea0f107779
Ugo <okekeugochukwu.dev@gmail.com>
2021-08-18 17:15:07 +0100

    done with aside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 document.getElementById("hamburger-close").addEventListener("click", () => {
     document.getElementById("sidebar").style.marginLeft = "-100%";
 })
+
+document.getElementById("hamburger-open").addEventListener("click", () => {
+    document.getElementById("sidebar").style.marginLeft = "0";
+})
